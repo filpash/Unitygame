e7fd7a0843e644e1fb674ef0b357ce2f146682af
Filipp <fili92@mail.ru>
2020-05-31 12:00:43 +0300

    first commit

---
+++ Assets/Scripts/MenuControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void SettingsPressed()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+        SceneManager.LoadScene("Auth Menu");
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    public void BackPressed()
+    {
+        Application.Quit();
+        SceneManager.LoadScene("Menu");
+    }
+}
